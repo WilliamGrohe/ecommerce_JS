71131c3c3a5dfdb4494b24f01aaf3b7a37cfeba7
WilliamGrohe <william.grohe@gmail.com>
2023-08-30 23:07:47 -0300

    organizando estilo dos cards de produtos

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ const catalogo = [
 ];
 
 for (const produtoCatalogo of catalogo){
-  const cartaoProduto = `<div class="card-produto" class="w-20">
+  const cartaoProduto = `<div class="border-solid border-2 border-sky-500 w-48 p-2" id="card-produto">
           <img src="./assets/img/${produtoCatalogo.imagem}" alt="Texto" style="height: 200px;">
           <p>${produtoCatalogo.marca}</p>
           <p>${produtoCatalogo.nome}</p>
